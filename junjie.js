1f2415fab6d8b2c2c4fc53e61d986a6b90c554e1
烧了个麦 <49426096+lj1026203182@users.noreply.github.com>
2024-03-09 18:44:58 +0800

    Update junjie.js

--- junjie.js
+++ junjie.js
@@ -1,3 +1,29 @@
+/*
+ *
+ *
+脚本功能：鱿鱼视频解锁会员
+软件版本：1.1.1
+下载地址：https://shrtm.nu/yysp
+脚本作者：Hausd0rff
+更新时间：2022-01-04
+电报频道：https://t.me/yqc_123
+问题反馈：https://t.me/yqc_777
+使用声明：⚠️此脚本仅供学习与交流，
+        请勿转载与贩卖！⚠️⚠️⚠️
+*******************************
+
+[rewrite_local]
+
+# > 鱿鱼视频解锁会员
+^https?:\/\/student-api\.iyincaishijiao\.com\/ep\/web\/trade\/is_bought* url script-response-body junjie.js
+[mitm] 
+
+hostname = *.iyincaishijiao*
+*
+*
+*/
+
+
 var objc = JSON.parse($response.body);
 console.log('objc: ', $response.body);
 objc.data.is_bought = true;
